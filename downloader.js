b57bb9a795aee321c9119093f7c94df25bba1ab6
Avidan Borisov <Avidanborisov@gmail.com>
2016-11-07 13:30:53 +0200

    Create downloader.js

---
+++ downloader.js
@@ -0,0 +1,24 @@
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+$(document).ready(function() {
+    $("#download").on("click", function(e) {
+        var url = $("#url").val()
+        var id = getParameterByName(url, "id");
+        if (!id) {
+            alert("Invalid Panopto URL!");
+            $(this).attr("href", "#");
+            return;
+        }
+
+        var video = "https://panoptotech.technion.ac.il/Panopto/Podcast/Embed/" + id + ".mp4";
+        $(this).attr("href", video);
+    })
+})
